c14782ed5808d707375cb52be8c847e2d579a39f
Devaarth Sharma <devaarth@gmail.com>
2019-11-19 12:13:42 +0530

    Material components imports

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatIconModule, MatCardModule],
   declarations: [SearchBarComponent, SideNavComponent],
   exports: [SearchBarComponent, SideNavComponent]
 })
-export class UiModule {}
+export class UiModule { }
